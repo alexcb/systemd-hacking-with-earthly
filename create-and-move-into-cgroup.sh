bb9b4f3cd910a747de83c42a82def55f1bd6cbc0
Alex Couture-Beil <alex@earthly.dev>
2022-02-23 17:01:23 -0800

    wip

--- create-and-move-into-cgroup.sh
+++ create-and-move-into-cgroup.sh
@@ -3,6 +3,10 @@ set -ex
 
 test "$$" = "1"
 
+if [ -f "/sys/fs/cgroup/cgroup.controllers" ]; then
+    echo 'INFO: detected cgroup v2'
+fi
+
 ls -la /sys/fs/cgroup
 mkdir /sys/fs/cgroup/foo.scope
 echo 1 > /sys/fs/cgroup/foo.scope/cgroup.procs
